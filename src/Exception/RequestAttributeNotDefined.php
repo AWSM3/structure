e10fdd5acf68ee73733237e4b34f649d8e3798ac
Dmitry <dmitry2500u@bk.ru>
2018-03-23 01:32:15 +0300

    Эксепшен используемый при неопределённости аттрибута Запроса

---
+++ src/Exception/RequestAttributeNotDefined.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @filename: RequestAttributeNotDefined.php
+ */
+declare(strict_types=1);
+
+/** @namespace */
+namespace Awsm3\Structure\Exception;
+
+/**
+ * Class RequestAttributeNotDefined
+ * @package Awsm3\Structure\Exception
+ */
+class RequestAttributeNotDefined extends \RuntimeException
+{
+
+}
